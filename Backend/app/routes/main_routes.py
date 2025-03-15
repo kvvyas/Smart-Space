d855a308edd8677cd121bd2b2e62595beffa22fe
Jerome Kithinji <kithinjijerome@gmail.com>
2025-03-15 19:58:00 -0400

    course details API fetch route added

--- Backend/app/routes/main_routes.py
+++ Backend/app/routes/main_routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify, request
 from flask_cors import CORS
+import requests
+
 
 main = Blueprint('main', __name__)
 
@@ -17,4 +19,40 @@ def get_about():
 @main.route('/contactus')  
 def get_contact():
     return jsonify({"phoneNumber": "911"})
+
+# API request to fetch and return course schedule details from Concordia API.
+@main.route('/courseDetails')
+def get_course_details():
+
+    url = "https://opendata.concordia.ca/API/v1/course/schedule/filter/*/*/*"
+    payload = {}
+    headers = {
+      'Authorization': 'Basic ODUzOmJiYjAxYjE4NDM3ODY2NGJjMDVkYWIyNGIxYmUzODdj'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    if response.status_code == 200:
+        data = response.json()  # Parse JSON response
+        
+        schedule_list = []
+        for course in data:
+            schedule_list.append({
+                "departmentCode": course.get("departmentCode", ""),
+                "currentEnrollment": int(course.get("currentEnrollment", "0")),
+                "locationCode": course.get("locationCode", ""),
+                "roomCode": course.get("roomCode", ""),
+                "buildingCode": course.get("buildingCode", ""),
+                "mondays": course.get("modays", "N"),
+                "tuesdays": course.get("tuesdays", "N"),
+                "wednesdays": course.get("wednesdays", "N"),
+                "thursdays": course.get("thursdays", "N"),
+                "fridays": course.get("fridays", "N"),
+                "saturdays": course.get("saturdays", "N"),
+                "sundays": course.get("sundays", "N")
+            })
+        
+        return jsonify(schedule_list)
+    else:
+        return jsonify({"error": f"Failed to fetch data, status code: {response.status_code}"}), response.status_code
 
